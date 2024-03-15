2eb161711316567f3eadb2ac27ae41ceb629c480
AMCrd <130474937+AMCrd@users.noreply.github.com>
2024-03-15 20:48:26 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -103,11 +103,11 @@ def calculate_serp_rating(final_results, sections_info):
         current_multipliers = POSITION_MULTIPLIERS
     
     serp_rating = sum([
-        sections_info['ads']['count'] * 3,
-        sections_info['related_questions']['count'] * 1.3,
-        sections_info['answer_box']['count'] * 1.2,
-        sections_info['discussions_and_forums']['count'] * 1.2,
-        sections_info['knowledge_graph']['count'] * 3
+        sections_info['ads']['count'] * 2,
+        sections_info['related_questions']['count'] * 1.15,
+        sections_info['answer_box']['count'] * 4,
+        sections_info['discussions_and_forums']['count'] * 1.15,
+        sections_info['knowledge_graph']['count'] * 4
     ])
     
     for result in final_results[:10]:
@@ -153,7 +153,7 @@ if query != "" and SERP_API_KEY != "":
         serp_rating_score = calculate_serp_rating(final_results, sections_info) * 2
 
         # Scaling SERP Rating Score to CliQ KD
-        cliq_kd = (serp_rating_score - 29.4) / (139.95 - 29.4) * 100
+        cliq_kd = (serp_rating_score - 27.8) / (128.5 - 27.8) * 100
 
         # Cliq kd output and message
         st.header(f"CliQ KD for '{query}' in {location}: {cliq_kd:.2f}")
